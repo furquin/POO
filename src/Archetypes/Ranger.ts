b6e00deccaaf1a0bb07535bb90d61cfa75f6fc94
furquin <laert.ff@gmail.com>
2022-05-31 16:21:09 -0300

    feat: implementa classe ranger

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,22 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
+
+export default class Ranger extends Archetype {
+  private _EnergyType: EnergyType;
+  private static _instanceArch = 0;
+
+  constructor(name: string) {
+    super(name);
+      
+    this._EnergyType = 'stamina';
+  }
+
+  get energyType(): EnergyType {
+    return this._EnergyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    this._instanceArch += 1;
+    return this._instanceArch;
+  }
+} 
