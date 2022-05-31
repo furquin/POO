888de260427ebd00df1cb28f1aff61a4ce716f95
furquin <laert.ff@gmail.com>
2022-05-30 22:37:57 -0300

    feat: implementa classe halfling

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,11 +1,12 @@
 import Race from './Race';
 import Dwarf from './Dwarf';
 import Elf from './Elf';
+import Halfling from './Halfling';
 
 export {
   Dwarf,
   Elf,
-
+  Halfling,
 };
 
 export default Race;
